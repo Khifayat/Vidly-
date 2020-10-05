233413ae7b9183e6e6ae7fb61ee02c1d93e4625a
Khifayat Abolurin <abolurinkhifayat1@gmail.com>
2020-10-05 10:47:13 -0400

    completed get,post,put,and delete operations

--- app.js
+++ app.js
@@ -13,24 +13,48 @@ const genres = [
   {id:3, name: 'Comedy'}
 ]
 
-// returns the list of genres when requested.
 app.get('/api/genres', (req,res) =>{
   res.send(genres);
 });
-// returns one specific genre when requested by its id
+
 app.get('/api/genres/:id', (req, res) =>{
  const genre = genres.find(c = c.id === parseInt(req.params.id));
- if(!genre) res.status(404).send('The genre with the given id was not found')// 404 Error
+ if(!genre) return res.status(404).send('The genre with the given id was not found')// 404 Error
  res.send(genre);
 });
 
 app.post('/api/genres', (req, res) =>{
   const{error} = validateGenre(req.body);
   if(error) return res.status(400).send(error.details[0].message);
+
+  const genre = {
+    id:genres.length+1,
+    name : req.body.name
+    };
+  genres.push(genre);
+    res.send(genre);
+
+});
+
+app.put('/api/genres/:id', (req,res) =>{
+  const cgenre = genres.find(c => c.id === parseInt(req.params.id))
+  if(!genre) res.status(404).send('The genre with the given id was not found')
+const {error} = validateGenre(req.body); // result.error
+  if (error) return res.status(400).send(error.details[0].message);
+
   genre.name = req.body.name
   res.send(genre);
+});
 
-})
+app.delete('/api/genre/:id', (req,res)=>{
+const genre = genre.find(c =>c.id === parseInt(req.params.id));
+if(!genre) return res.status(404).send('The genre with the given id was not found')
+
+const index = genres.indexOf(genre);
+genre.splice(index,1);
+
+res.send(genre);
+});
 
 function validateGenre(genre) {
   const schema = {
